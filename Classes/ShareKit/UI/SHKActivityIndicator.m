47d315c7d3b626ec13b0ed87e15929070629a806
k-be <scalli-k-be@ya.ru>
2013-08-22 13:16:29 +0700

    changes for removing memory warnings

--- Classes/ShareKit/UI/SHKActivityIndicator.m
+++ Classes/ShareKit/UI/SHKActivityIndicator.m
@@ -246,7 +246,7 @@
 {
 	if (self.progress == nil)
 	{
-        self.progress = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
+        self.progress = [[[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar] autorelease];
         
 		self.progress.frame = CGRectMake(15.0f,
                                    15.0f,
